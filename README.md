093495606122043b6abdabdf635cd28dfe0e7461
yigtuyumz <yigtuyumz@gmail.com>
2024-02-10 01:26:42 +0300

    api src upgrade

--- README.md
+++ README.md
@@ -19,3 +19,94 @@ Sec-Fetch-Dest: document
 Accept-Encoding: gzip, deflate, br
 Accept-Language: de-DE,de;q=0.9,en-US;q=0.8,en;q=0.7
 ```
+
+## Makefile
+```sh
+make all        # release build
+make debug      # debug build
+make clean      # remove output files
+make run        # run executable only
+make re         # clean all
+make redebug    # clean debug run
+```
+
+
+
+## Variables
+
+
+```c
+
+#define __DEBUG__ 1
+
+#if __DEBUG__
+# define DEBUG_PORT 8080
+# define DEBUG_CLIDATA_BUFFSZ 9999
+#endif
+
+
+#define MAX_REQUEST_QUEUE 5
+
+
+#define ERR_CODE_NO_BINDPORT 1
+#define ERR_CODE_CREATE_APISOCK 2
+#define ERR_CODE_APIBINDPORT_OUT_OF_RANGE 3
+#define ERR_CODE_APIBIND_FAIL 4
+#define ERR_CODE_APILISTEN_FAIL 5
+#define ERR_CODE_ACCEPTSOCK_FAIL 6
+#define ERR_CODE_ACCEPTSOCK_SETLINGER_FAIL 7
+#define ERR_CODE_ACCEPTSOCK_READ_FAIL 8
+#define ERR_CODE_ACCEPTSOCK_SEND_FAIL 9
+#define ERR_CODE_ACCEPTSOCK_CLOSE 10
+#define ERR_CODE_APISOCK_CLOSE 11
+#define ERR_CODE_MALLOC_FAIL 12
+
+
+int api_bindport;
+int APISOCK = socket(AF_INET, SOCK_STREAM, 0);
+struct sockaddr_in api_address;
+    api_address.sin_family = AF_INET;
+    api_address.sin_addr.s_addr = INADDR_ANY;
+    api_address.sin_port = htons(api_bindport);
+int api_bind = bind(APISOCK, (struct sockaddr *)&api_address, sizeof(api_address));
+int api_listen = listen(APISOCK, MAX_REQUEST_QUEUE);
+char http_header_response[] = "HTTP/1.1 200 OK\r\n ... { \"message\": \"wagabond capi\" }\r\n\0";
+size_t response_len = strlen(http_header_response);
+
+
+
+struct sockaddr_in cli_address;
+size_t cli_addrlen = sizeof(cli_address);
+int ACCEPTSOCK;
+int accept_setopt;
+char *DYN_buff = (char *) malloc(DEBUG_CLIDATA_BUFFSZ);
+
+int acceptsock_readval;
+int a;
+int close_acceptsock;
+struct linger acceptsock_ling;
+    acceptsock_ling.l_onoff = 1;
+    acceptsock_ling.l_linger = 0;
+
+int requested_urllength;
+char *DYN_requested_url;
+
+while (1)
+{
+    ACCEPTSOCK = accept(APISOCK, (struct sockaddr *)&cli_address,  (socklen_t *)&cli_addrlen);
+    accept_setopt = setsockopt(ACCEPTSOCK, SOL_SOCKET, SO_LINGER, &acceptsock_ling, sizeof(struct linger));
+    acceptsock_readval = read(ACCEPTSOCK, DYN_buff, DEBUG_CLIDATA_BUFFSZ);
+
+    requested_urllength = find_url_index(DYN_buff, " HTTP/1.1\r\n");
+    DYN_requested_url = (char *) malloc(sizeof(char) * i);
+
+    a = send(ACCEPTSOCK, block, blocklen, 0);
+    close_acceptsock = close(ACCEPTSOCK);
+}
+
+int close_apisock = close(APISOCK);
+
+
+```
+
+#### _**“We make an idol of our fear and that idol we call God.”**_
--- api/src/main.c
+++ api/src/main.c
@@ -1,10 +1,13 @@
-#include <stdlib.h>        /* exit() */
-#include <stdio.h>         /* printf() family */
-#include <unistd.h>        /* read(), close() etc. */
+#include <stdlib.h>           /* exit() */
+#include <stdio.h>            /* printf() family */
+#include <unistd.h>           /* read(), close() etc. */
 // #include <sys/types.h>     /* socket options */
-#include <sys/socket.h>    /* socket() */
-#include <arpa/inet.h>     /* struct sockaddr_in */
-#include <string.h>        /* strlen() */
+#include <sys/socket.h>       /* socket() */
+#include <arpa/inet.h>        /* struct sockaddr_in */
+#include <stdarg.h>           /* variadic */
+#include <string.h>           /* str....() */
+
+
 
 #define __DEBUG__ 1
 
@@ -23,9 +26,138 @@
 #define ERR_CODE_APIBIND_FAIL 4
 #define ERR_CODE_APILISTEN_FAIL 5
 #define ERR_CODE_ACCEPTSOCK_FAIL 6
-#define ERR_CODE_ACCEPTSOCK_READ_FAIL 7
-#define ERR_CODE_ACCEPTSOCK_CLOSE 8
-#define ERR_CODE_APISOCK_CLOSE 9
+#define ERR_CODE_ACCEPTSOCK_SETLINGER_FAIL 7
+#define ERR_CODE_ACCEPTSOCK_READ_FAIL 8
+#define ERR_CODE_ACCEPTSOCK_SEND_FAIL 9
+#define ERR_CODE_ACCEPTSOCK_CLOSE 10
+#define ERR_CODE_APISOCK_CLOSE 11
+#define ERR_CODE_MALLOC_FAIL 12
+
+void
+utils_putchar(int fd, char c)
+{
+	write(fd, &c, 1);
+}
+
+size_t
+utils_strlen(const char *str)
+{
+	size_t len = 0;
+	while (*(str + len)) {
+		len++;
+	}
+
+	return (len);
+}
+
+void
+utils_putstr(int fd, const char *str)
+{
+	write(fd, str, utils_strlen(str));
+}
+
+void
+utils_putnbr(int fd, int nb)
+{
+	if (nb < 0) {
+		utils_putchar(fd, '-');
+		if (nb == ~0x7FFFFFFF) {
+			nb = -147483648;
+			utils_putchar(fd, '2');
+		}
+		nb *= -1;
+	}
+	if (nb >= 10) {
+		utils_putnbr(fd, (nb / 10));
+		nb = (nb % 10);
+	}
+	if (nb < 10) {
+		utils_putchar(fd, (nb + 48));
+	}
+}
+
+
+void *
+utils_memset(void *dest, int c, size_t n)
+{
+	while (n--) {
+		*((unsigned char *)dest + n) = c;
+	}
+	return (dest);
+}
+
+void
+message(char *fmt, ...)
+{
+	va_list ap;
+	char c;
+	char *s;
+    int d;
+
+    va_start(ap, fmt);
+    while (*fmt) {
+        if (*fmt == '~') {
+            fmt++;
+            if (*fmt == 'E') {
+                utils_putstr(STDERR_FILENO, "\033[0;30m[ ERROR ]\033[0m");
+            } else if (*fmt == 'W') {
+                utils_putstr(STDOUT_FILENO, "\033[0;31m[ WARNING ]\033[0m");
+            } else if (*fmt == 'S') {
+                utils_putstr(STDOUT_FILENO, "\033[0;32m[ SUCCESS ]\033[0m");
+            } else if (*fmt == 'D') {
+                utils_putstr(STDOUT_FILENO, "\033[0;33m[ DEBUG ]\033[0m");
+            } else if (*fmt == 's') {
+                s = va_arg(ap, char *);
+                utils_putstr(STDOUT_FILENO, s);
+            } else if (*fmt == 'd') {
+                d = va_arg(ap, int);
+                utils_putnbr(STDOUT_FILENO, d);
+            } else if (*fmt == 'c') {
+                c = va_arg(ap, int);
+                utils_putchar(STDOUT_FILENO, c);
+            }
+        } else {
+                utils_putchar(STDOUT_FILENO, *fmt);
+            }
+        fmt++;
+    }
+
+    va_end(ap);
+}
+
+int
+utils_strncmp(const char *a, const char *b, size_t n)
+{
+	size_t i = 0;
+	while (*(a + i) && (*(a + i) == *(b + i)) && i < n) {
+		i++;
+	}
+
+	if (i == n) {
+		return (0);
+	}
+
+	return (*(a + i) - *(b + i));
+}
+
+int
+find_url_index(char req[], char *search)
+{
+    size_t i = 0;
+    size_t j;
+    while (req[i]) {
+        j = 0;
+        while (req[i + j] == *(search + j) && *(search + j)) {
+            j++;
+        }
+        if (*(search + j) == 0) {
+            return (i);
+        }
+        i++;
+    }
+    return (0);
+}
+
 
 /*
  * This is the main source file of a minimalized API for simulating basic
@@ -35,42 +167,40 @@
 int
 main(int argc, char *argv[])
 {
-    char *debuginfo_str = "[ DEBUG_INFO ] \0";
-    int api_bindport;
 
+    int api_bindport;
 
 #if __DEBUG__
     api_bindport = DEBUG_PORT;
 #else
     if (argc < 2) {
-        dprintf(STDERR_FILENO, "No binding port for socket! [ 1000 - 65000 ]\n");
+        message("~E CAPI needs a port number to run properly.\n");
         exit(ERR_CODE_NO_BINDPORT);
     }
 
     api_bindport = atoi(argv[1]);
+
     if (api_bindport < 1000 || api_bindport > 65000) {
-        dprintf(STDERR_FILENO, "Given port number is out of range! [ 1000 - 65000 ]\n");
+        message("~E Given port number is out of range! [ 1000 - 65000 ]\n");
         exit(ERR_CODE_APIBINDPORT_OUT_OF_RANGE);
     }
 #endif
 
 
     int APISOCK = socket(AF_INET, SOCK_STREAM, 0);
-
-
     if (APISOCK < 0) {
-        dprintf(STDERR_FILENO, "ERROR while creating API socket.\n");
+        message("~E CAPI socket can not be created.\n");
         exit(ERR_CODE_CREATE_APISOCK);
     }
 
 
 #if __DEBUG__
-    dprintf(STDOUT_FILENO, "%sAPISOCK created.\n", debuginfo_str);
+    message("~D APISOCK created.\n");
 #endif
 
     /*
         TODO
-        setsockopt
+        set APISOCK options via setsockopt
     */
 
 
@@ -84,188 +214,172 @@ main(int argc, char *argv[])
     int api_bind = bind(APISOCK, (struct sockaddr *)&api_address, sizeof(api_address));
 
 #if __DEBUG__
-    dprintf(STDOUT_FILENO, "%sINADDR_ANY binded to APISOCK.\n", debuginfo_str);
+    message("~D INADDR_ANY binded to APISOCK.\n");
 #endif
 
     if (api_bind < 0) {
-        dprintf(STDERR_FILENO, "Binding api_address to APISOCK has failed.\n");
+        message("~E Binding api_address to APISOCK has failed.\n");
         exit(ERR_CODE_APIBIND_FAIL);
     }
 
 
     int api_listen = listen(APISOCK, MAX_REQUEST_QUEUE);
     if (api_listen < 0) {
-        dprintf(STDERR_FILENO, "APISOCK listen has failed.\n");
+        message("~E APISOCK listen has failed.\n");
         exit(ERR_CODE_APILISTEN_FAIL);
     }
 
 #if __DEBUG__
-    dprintf(STDOUT_FILENO, "%sAPISOCK listens incoming connection requests.\n", debuginfo_str);
+    message("~D APISOCK listens incoming connection requests.\n");
 #endif
 
 
-
-// TODO    while (1) {
-// TODO        struct sockaddr_in client_address;
-// TODO        int client_addrlen = sizeof(client_address);
-// TODO        int ACCEPTSOCK = accept(APISOCK, (struct sockaddr *)&client_address, (socklen_t *)&client_addrlen);
-// TODO
-// TODO        if (ACCEPTSOCK < 0) {
-// TODO            perror("Connection request to APISOCK has failed.");
-// TODO            exit(ERR_CODE_ACCEPTSOCK_FAIL);
-// TODO        }
-// TODO
-// TODO        char buff[1024] = {0};
-// TODO        int acceptsock_readval = read(ACCEPTSOCK, buff, sizeof(buff));
-// TODO        if (acceptsock_readval < 0) {
-// TODO            perror("Error while ACCEPTSOCK is reading from client.");
-// TODO            exit(ERR_CODE_ACCEPTSOCK_READ_FAIL);
-// TODO        }
-// TODO
-// TODO        printf("Received request from client:\n%s\n", buff);
-// TODO
-// TODO        const char *http_response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 12\r\n\r\nHello, World!";
-// TODO        int send_result = send(ACCEPTSOCK, http_response, strlen(http_response), 0);
-// TODO
-// TODO        if (send_result < 0) {
-// TODO            perror("Failed to send response to client.");
-// TODO            exit(ERR_CODE_SEND_FAIL);
-// TODO        }
-// TODO
-// TODO        close(ACCEPTSOCK);
-// TODO    }
-
-// ! /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
     // JSON
     // const char *http_header_response = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 30\r\n\r\n\0";
 
-    // CORS
-    const char *http_header_response = "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept\r\nContent-Type: text/plain\r\nContent-Length: 30\r\n\r\n\0";
-    const char *http_response_body = "{ \"message\": \"wagabond capi\" }\r\n\0";
-    size_t blocklen = strlen(http_header_response) + strlen(http_response_body) + 1;
-    char *block = (char *) malloc(blocklen);
-    strcat(block, http_header_response);
-    strcat(block, http_response_body);
+    // CORS HEADER RESPONSE + RESPONSE BODY
+    char http_header_response[] = "HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nAccess-Control-Allow-Methods: GET, POST, PUT, DELETE\r\nAccess-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept\r\nContent-Type: text/plain\r\nContent-Length: 30\r\n\r\n{ \"message\": \"wagabond capi\" }\r\n";
+    size_t response_len = strlen(http_header_response);
+    // SO_LINGER seçeneğini etkinleştir
+// !    struct linger ling;
+// !    ling.l_onoff = 1; // Beklemeyi etkinleştir
+// !    ling.l_linger = 0; // Bekleme süresi (saniye cinsinden), sıfır olması soketi hemen kapatır
+
+
+
+
+
+
+
+
+    struct sockaddr_in cli_address;
+    size_t cli_addrlen;
+    int ACCEPTSOCK;
+    int accept_setopt;
+    char *DYN_buff = (char *) malloc(DEBUG_CLIDATA_BUFFSZ);
+
+    if (DYN_buff == NULL) {
+        message("~E Malloc failed for <<<<*DYN_buff>>>>\n");
+        exit(ERR_CODE_MALLOC_FAIL);
+    }
+
+    int acceptsock_readval;
+    int a;
+    int close_acceptsock;
+    struct linger acceptsock_ling;
+    acceptsock_ling.l_onoff = 1;
+    acceptsock_ling.l_linger = 0;
+
+
+
+    int requested_urllength;
+    char *DYN_requested_url;
 
 
     while (1) {
-        struct sockaddr_in cli_address;
-        size_t cli_addrlen = sizeof(cli_address);
-        int ACCEPTSOCK = accept(APISOCK, (struct sockaddr *)&cli_address,  (socklen_t *)&cli_addrlen);
+        ACCEPTSOCK = accept(APISOCK, (struct sockaddr *)&cli_address,  (socklen_t *)&cli_addrlen);
 
         if (ACCEPTSOCK < 0) {
-            dprintf(STDERR_FILENO, "Connection request from client to APISOCK has failed.\n");
+            message("~E Connection request from client to APISOCK has failed.\n");
             exit(ERR_CODE_ACCEPTSOCK_FAIL);
         }
 
+
+    // SO_LINGER seçeneğini etkinleştir
+// !    struct linger ling;
+// !    ling.l_onoff = 1; // Beklemeyi etkinleştir
+// !    ling.l_linger = 0; // Bekleme süresi (saniye cinsinden), sıfır olması soketi hemen kapatır
+// !    if (setsockopt(sockfd, SOL_SOCKET, SO_LINGER, &ling, sizeof(ling)) < 0) {
+// !        perror("setsockopt failed");
+// !        exit(EXIT_FAILURE);
+// !    }
+        accept_setopt = setsockopt(ACCEPTSOCK, SOL_SOCKET, SO_LINGER, &acceptsock_ling, sizeof(struct linger));
+        if (accept_setopt < 0) {
+            message("~E Accept socket set options linger failed.\n");
+            exit(ERR_CODE_ACCEPTSOCK_SETLINGER_FAIL);
+        }
+
+
+
+
 #if __DEBUG__
-        dprintf(STDOUT_FILENO, "%sAccept socket ACCEPTSOCK created successfully.\n", debuginfo_str);
+        message("~D Accept socket ACCEPTSOCK created successfully.\n");
 #endif
-        char buff[DEBUG_CLIDATA_BUFFSZ] = { 0 };
-        int acceptsock_readval = read(ACCEPTSOCK, buff, DEBUG_CLIDATA_BUFFSZ);
+        memset(DYN_buff, 0, DEBUG_CLIDATA_BUFFSZ);
+        acceptsock_readval = read(ACCEPTSOCK, DYN_buff, DEBUG_CLIDATA_BUFFSZ);
         if (acceptsock_readval < 0) {
-            dprintf(STDOUT_FILENO, "Error while ACCEPTSOCK is reading from client.\n");
+            message("~E Error while ACCEPTSOCK is reading buffer from client.\n");
             exit(ERR_CODE_ACCEPTSOCK_READ_FAIL);
         }
 
-        
+
+
+        requested_urllength = find_url_index(DYN_buff, " HTTP/1.1\r\n");
+        DYN_requested_url = (char *) malloc(sizeof(char) * requested_urllength);
+        strncpy(DYN_requested_url, DYN_buff, requested_urllength);
+
+        message("~S Requested <<<~s>>>.\n", DYN_requested_url);
+
+        if (strncmp(DYN_buff, "GET / HTTP/1.1", 14) != 0) {
+            if (strncmp(DYN_buff, "GET /break", 10) == 0) {
+                message("~W Requested <<'/break'>> Breaking the law !\n");
+                close(ACCEPTSOCK);
+                break ;
+            } else {
+                close(ACCEPTSOCK);
+                continue ;
+            }
+        }
+
+
+
+        free(DYN_requested_url);
+
 #if __DEBUG__
-        dprintf(STDOUT_FILENO, "%sACCEPTSOCK Buffer from Client data\n================\n%s================\n", debuginfo_str, buff);
+        message("~D ACCEPTSOCK Buffer from Client data\n~s", DYN_buff);
 #endif
 
-        if (strncmp(buff, "GET / HTTP/1.1", 14) != 0) {
-            dprintf(STDERR_FILENO, "Requested another location than '/', skipping.\n");
-            close(ACCEPTSOCK);
-            continue ;
-        }
 
 
-        printf("%s\n", block);
-        int a = send(ACCEPTSOCK, block, blocklen, 0);
+        a = send(ACCEPTSOCK, http_header_response, response_len, 0);
         if (a < 0) {
-            dprintf(STDERR_FILENO, "send error\n");
+            message("~E send error\n");
+            exit(ERR_CODE_ACCEPTSOCK_SEND_FAIL);
         }
-        
 
 
-        int close_acceptsock = close(ACCEPTSOCK);
+
+
+        close_acceptsock = close(ACCEPTSOCK);
         if (close_acceptsock < 0) {
-            dprintf(STDERR_FILENO, "ACCEPTSOCK close failed.\n");
+            message("~E ACCEPTSOCK close failed.\n");
             exit(ERR_CODE_ACCEPTSOCK_CLOSE);
         }
 #if __DEBUG__
-        dprintf(STDOUT_FILENO, "%sACCEPTSOCK closed successfully.\n", debuginfo_str);
+        message("~D ACCEPTSOCK closed successfully.\n");
 #endif
     }
 
+    free(DYN_buff);
 
 
-    free(block);
 
-// !     // accept creates a new socket!
-// !     size_t api_addrlen = sizeof(api_address);
-// ! 
-// !     int ACCEPTSOCK = accept(APISOCK, (struct sockaddr *)&api_address, (socklen_t *)&api_addrlen);
-// !     if (ACCEPTSOCK < 0) {
-// !         dprintf(STDERR_FILENO, "Connection request to APISOCK has failed.\n");
-// !         exit(ERR_CODE_ACCEPTSOCK_FAIL);
-// !     }
-// ! 
-// ! #if __DEBUG__
-// !     dprintf(STDOUT_FILENO, "%sAccept socket ACCEPTSOCK created successfully.\n", debuginfo_str);
-// ! #endif
-// ! 
-// !     char buff[DEBUG_CLIDATA_BUFFSZ] = { 0 };
-// !     int acceptsock_readval = read(ACCEPTSOCK, buff, DEBUG_CLIDATA_BUFFSZ);
-// !     if (acceptsock_readval < 0) {
-// !         dprintf(STDOUT_FILENO, "Error while ACCEPTSOCK is reading from client.\n");
-// !         exit(ERR_CODE_ACCEPTSOCK_READ_FAIL);
-// !     }
-// ! 
-// ! #if __DEBUG__
-// !     dprintf(STDOUT_FILENO, "%sACCEPTSOCK Buffer from Client data\n================\n%s================\n", debuginfo_str, buff);
-// ! #endif
-// ! 
-// ! 
-// !     const char *http_header_response = "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nContent-Length: 30\r\n\r\n\0";
-// !     const char *http_response_body = "{ \"message\": \"Hello, world!\" }\r\n\0";
-// !     size_t blocklen = strlen(http_header_response) + strlen(http_response_body) + 1;
-// !     char *block = (char *) malloc(blocklen);
-// ! 
-// !     strcat(block, http_header_response);
-// !     strcat(block, http_response_body);
-// !     printf("%s\n", block);
-// !     int a = send(ACCEPTSOCK, block, blocklen, 0);
-// !     if (a < 0) {
-// !         dprintf(STDERR_FILENO, "send error\n");
-// !     }
-// !     free(block);
-// ! 
-// ! 
-// !     int close_acceptsock = close(ACCEPTSOCK);
-// !     if (close_acceptsock < 0) {
-// !         dprintf(STDERR_FILENO, "ACCEPTSOCK close failed.\n");
-// !         exit(ERR_CODE_ACCEPTSOCK_CLOSE);
-// !     }
-// ! #if __DEBUG__
-// !     dprintf(STDOUT_FILENO, "%sACCEPTSOCK closed successfully.\n", debuginfo_str);
-// ! #endif
 
-// ! /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 
 
     int close_apisock = close(APISOCK);
     if (close_apisock < 0) {
-        dprintf(STDERR_FILENO, "APISOCK close failed.\n");
+        message("~E APISOCK close failed.\n");
         exit(ERR_CODE_APISOCK_CLOSE);
     }
 #if __DEBUG__
-    dprintf(STDOUT_FILENO, "%sAPISOCK closed successfully.\n", debuginfo_str);
+    message("~D APISOCK closed successfully.\n");
 #endif
 
 
     return (0);
 }
-
